701f3cd0c7e3f48d771e21a5325ffe0b6b83e6c5
AJ <ancheetah@users.noreply.github.com>
2023-03-06 23:23:32 -0500

    accordion expand icons

--- src/components/AccordionItem.jsx
+++ src/components/AccordionItem.jsx
@@ -2,7 +2,9 @@ import { useState } from 'react';
 import PropTypes from 'prop-types';
 import styles from '@/styles/AccordionItem.module.css';
 
-import UserSvg from '/public/icons/user.svg'
+import UserSvg from '/public/icons/user.svg';
+import ExpandLess from '/public/icons/expand_less.svg';
+import ExpandMore from '/public/icons/expand_more.svg';
 export default function AccordionItem({ user }) {
     const [isActive, setIsActive] = useState(false);
 
@@ -28,25 +30,35 @@ export default function AccordionItem({ user }) {
                 onClick={() => setIsActive(!isActive)}
             >
                 <div className={styles.headerLeft}>
-                  <UserSvg width={50} height={50} alt='user icon'/>
-                  <div class={styles.headerText}>
-                    <div className={styles.userName}>{`${user.firstName} ${user.lastName}`}</div>
-                    <div className={styles.userRole}>{user.role}</div>
-                    <div className={styles.userEmail}>{user.email}</div>
-                  </div>
+                    <UserSvg width={50} height={50} alt='user icon' />
+                    <div class={styles.headerText}>
+                        <div
+                            className={styles.userName}
+                        >{`${user.firstName} ${user.lastName}`}</div>
+                        <div className={styles.userRole}>{user.role}</div>
+                        <div className={styles.userEmail}>{user.email}</div>
+                    </div>
                 </div>
-                <div>{isActive ? '-' : '+'}</div>
+                <div>{isActive ? <ExpandLess /> : <ExpandMore />}</div>
             </div>
             {isActive && (
                 <div className={styles.accordionContent}>
                     <div className={styles.detailHeader}>Address</div>
-                    <div className={styles.detailItem}>{`${user.street}, ${user.city}, ${user.state} ${user.zip}`}</div>
+                    <div
+                        className={styles.detailItem}
+                    >{`${user.street}, ${user.city}, ${user.state} ${user.zip}`}</div>
                     <div className={styles.detailHeader}>Phone</div>
-                    <div className={styles.detailItem}>{getPhone(user.phone)}</div>
+                    <div className={styles.detailItem}>
+                        {getPhone(user.phone)}
+                    </div>
                     <div className={styles.detailHeader}>Created At</div>
-                    <div className={styles.detailItem}>{getFormattedDate(user.createdAt)}</div>
+                    <div className={styles.detailItem}>
+                        {getFormattedDate(user.createdAt)}
+                    </div>
                     <div className={styles.detailHeader}>Last Logged In</div>
-                    <div className={styles.detailItem}>{getFormattedDate(user.lastLoggedIn)}</div>
+                    <div className={styles.detailItem}>
+                        {getFormattedDate(user.lastLoggedIn)}
+                    </div>
                 </div>
             )}
         </div>
@@ -54,18 +66,18 @@ export default function AccordionItem({ user }) {
 }
 
 AccordionItem.propTypes = {
-  user: PropTypes.shape({
-    id: PropTypes.number,
-    firstName: PropTypes.string.isRequired,
-    lastName: PropTypes.string.isRequired,
-    role: PropTypes.string.isRequired,
-    email: PropTypes.string.isRequired,
-    street: PropTypes.string.isRequired,
-    city: PropTypes.string.isRequired,
-    state: PropTypes.string.isRequired,
-    zip: PropTypes.string.isRequired,
-    phone: PropTypes.string.isRequired,
-    createdAt: PropTypes.string.isRequired,
-    lastLoggedIn: PropTypes.string.isRequired,
-  })
-}
+    user: PropTypes.shape({
+        id: PropTypes.number,
+        firstName: PropTypes.string.isRequired,
+        lastName: PropTypes.string.isRequired,
+        role: PropTypes.string.isRequired,
+        email: PropTypes.string.isRequired,
+        street: PropTypes.string.isRequired,
+        city: PropTypes.string.isRequired,
+        state: PropTypes.string.isRequired,
+        zip: PropTypes.string.isRequired,
+        phone: PropTypes.string.isRequired,
+        createdAt: PropTypes.string.isRequired,
+        lastLoggedIn: PropTypes.string.isRequired
+    })
+};
